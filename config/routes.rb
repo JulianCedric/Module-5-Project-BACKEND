8568878d8a24267ce1a051f5a3e653ad448184dd
JulianCedric <julian.pormentilla@gmail.com>
2020-09-12 13:53:42 -0400

    Resolving fetch issue with new seeds (in progress 2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# Rails.application.routes.draw do
+#   namespace :api do
+#     namespace :v1 do  
+#       resources :reminders
+#       resources :habits
+#       resources :users
+#     end 
+#   end 
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
+
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do  
@@ -8,7 +19,3 @@ Rails.application.routes.draw do
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
-# 
-
-# 
