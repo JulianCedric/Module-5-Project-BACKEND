37578bd8914c954caf37b1768cde25068d22486e
JulianCedric <julian.pormentilla@gmail.com>
2020-09-28 02:08:58 -0400

    Added  more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,6 +22,90 @@ Habit.create(
     counter: 0,
     percentage: 0,
     dailyWinConfirmation: false
-    )
+)
+
+Habit.create(
+    user_id: 1,
+    name: '',
+    description: 'Practice piano for at least 20 minutes',
+    quote: "'Successful people are simply those with successful habits.' -Brian Tracy",
+    dateCreated: '',
+    column: 'A',
+    counter: 0,
+    percentage: 0,
+    dailyWinConfirmation: false
+)
+
+Habit.create(
+    user_id: 1,
+    name: '',
+    description: 'Read at least 3 pages of a fiction novel',
+    quote: "'The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking.' -Albert Einstein",
+    dateCreated: '',
+    column: 'A',
+    counter: 0,
+    percentage: 0,
+    dailyWinConfirmation: false
+)
+
+Habit.create(
+    user_id: 1,
+    name: '',
+    description: 'Water plants in my room!',
+    quote: "'Drop by drop is the water pot filled.' -BUDDHA",
+    dateCreated: '',
+    column: 'A',
+    counter: 20,
+    percentage: 90.00,
+    dailyWinConfirmation: false
+)
+
+Habit.create(
+    user_id: 1,
+    name: '',
+    description: 'Drink 1 more glass of water today',
+    quote: "'I fear not the man who has practiced 10,000 kicks, but I do fear the man who has practiced one kick 10,000 times.' -BRUCE LEE",
+    dateCreated: '',
+    column: 'A',
+    counter: 7,
+    percentage: 33.33,
+    dailyWinConfirmation: false
+)
+
+Habit.create(
+    user_id: 1,
+    name: '',
+    description: 'Make bed before morning jog',
+    quote: "'The chains of habit are too weak to be felt until they are too strong to be broken.'   --Samuel Johnson",
+    dateCreated: '',
+    column: 'C',
+    counter: 21,
+    percentage: 100,
+    dailyWinConfirmation: false
+)
+
+Habit.create(
+    user_id: 1,
+    name: '',
+    description: 'Prep gym clothes next to bed the night before a work-out day',
+    quote: "'Let today be the day you give up who you've been for who you can become.' -Hal Elrod",
+    dateCreated: '',
+    column: 'C',
+    counter: 21,
+    percentage: 100,
+    dailyWinConfirmation: false
+)
+
+Habit.create(
+    user_id: 1,
+    name: '',
+    description: 'Study / Practice Algorithms & Data Structures for at least an hour',
+    quote: "'It's not what we do once in a while that shapes our lives, but what we do consistently.' -Tony Robbins",
+    dateCreated: '',
+    column: 'C',
+    counter: 21,
+    percentage: 100,
+    dailyWinConfirmation: false
+)
 
 Reminder.create(habit_id: 1, duration: 21, startDate: 2020-8-25, endDate: 2020-9-15, stickifyd: false)
